891ade99e7f76f7cc715d0b8e338319e78c812d7
Felix Tenggren <felixtenggren1@gmail.com>
2024-07-07 09:13:10 +0000

    Use NavLink for dynamic active class changing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
@@ -15,12 +15,12 @@ const NavBar = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Link to="/" className="nav-link">
+              <NavLink to="/" className="nav-link">
                 Home
-              </Link>
-              <Link to="/signin" className="nav-link">
+              </NavLink>
+              <NavLink to="/signin" className="nav-link">
                 Sign in
-              </Link>
+              </NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
