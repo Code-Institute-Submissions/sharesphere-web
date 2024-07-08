85744044b1fbc9d67438710df14c25debb0203b2
Felix Tenggren <felixtenggren1@gmail.com>
2024-07-08 08:24:24 +0000

    Add error alerts for all signup fields

--- src/pages/authentication/SignUp.js
+++ src/pages/authentication/SignUp.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Alert, Col, Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import css from "../../styles/css/SignUp.module.css";
@@ -12,26 +12,26 @@ const SignUp = () => {
     password1: "",
     password2: "",
   });
-const {username, password1, password2} = signUpData
-const navigate = useNavigate()
+  const [fieldErrors, setFieldErrors] = useState([]);
+  const { username, password1, password2 } = signUpData;
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setSignUpData({
       ...signUpData,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-    await axios.post("/dj-rest-auth/registration/", signUpData)
-    navigate(-1)
+      await axios.post("/dj-rest-auth/registration/", signUpData);
+      navigate(-1);
     } catch (error) {
-      e = error.response?.data
-      console.log(e)
+      setFieldErrors(error.response?.data);
     }
-  }
+  };
 
   return (
     <Container className={css.FormWrapper}>
@@ -40,20 +40,74 @@ const navigate = useNavigate()
           <Form className={css.Form} onSubmit={handleSubmit}>
             <Form.Group className="mb-4" controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
-              <Form.Control type="text" placeholder="Username" value={username} onChange={handleChange} name="username" />
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={handleChange}
+                name="username"
+              />
+              {fieldErrors.username?.map((e, index) => (
+                <Alert
+                  key={`${e}-${index}`}
+                  variant="warning"
+                  className={css.FieldAlert}
+                >
+                  {e}
+                </Alert>
+              ))}
             </Form.Group>
 
             <Form.Group className="mb-4" controlId="password1">
               <Form.Label className="d-none">Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" value={password1} onChange={handleChange} name="password1" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password1}
+                onChange={handleChange}
+                name="password1"
+              />
+              {fieldErrors.password1?.map((e, index) => (
+                <Alert
+                  key={`${e}-${index}`}
+                  variant="warning"
+                  className={css.FieldAlert}
+                >
+                  {e}
+                </Alert>
+              ))}
             </Form.Group>
 
             <Form.Group className="mb-4" controlId="password2">
               <Form.Label className="d-none">Confirm password</Form.Label>
-              <Form.Control type="password" placeholder="Confirm password" value={password2} onChange={handleChange} name="password2" />
+              <Form.Control
+                type="password"
+                placeholder="Confirm password"
+                value={password2}
+                onChange={handleChange}
+                name="password2"
+              />
+              {fieldErrors.password2?.map((e, index) => (
+                <Alert
+                  key={`${e}-${index}`}
+                  variant="warning"
+                  className={css.FieldAlert}
+                >
+                  {e}
+                </Alert>
+              ))}
             </Form.Group>
+            {fieldErrors.non_field_errors?.map((e, index) => (
+              <Alert
+                key={`${e}-${index}`}
+                variant="warning"
+                className={css.FieldAlert}
+              >
+                {e}
+              </Alert>
+            ))}
 
-            <Button variant="success" type="submit">
+            <Button variant="success" type="submit" className="mt-3">
               Sign up!
             </Button>
           </Form>
