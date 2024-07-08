226d4c945e88a08d4b90fe1ce5c36b6126cc3feb
Felix Tenggren <felixtenggren1@gmail.com>
2024-07-08 09:33:05 +0000

    Add signin page and form functionality

---
+++ src/pages/authentication/SignIn.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import { Alert, Col, Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import css from "../../styles/css/SignUp.module.css";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
+const SignIn = () => {
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+  const [fieldErrors, setFieldErrors] = useState([]);
+  const { username, password } = signInData;
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setSignInData({
+      ...signInData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/login/", signInData);
+      navigate("/");
+    } catch (error) {
+      setFieldErrors(error.response?.data);
+    }
+  };
+
+  return (
+    <Container className={css.FormWrapper}>
+      <Row className="flex-grow-1">
+        <Col xs="12" className={css.PageHeader}>
+          <h1>Sign in to your account</h1>
+          <p>
+            If you don't already have an account, please{" "}
+            <Link to={"/signup"} className="text-decoration-none">
+              sign up
+            </Link>
+            .
+          </p>
+        </Col>
+        <Col md="6">
+          <Form className={css.Form} onSubmit={handleSubmit}>
+            <Form.Group className="mb-4" controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={handleChange}
+                name="username"
+              />
+              {fieldErrors.username?.map((e, index) => (
+                <Alert
+                  key={`${e}-${index}`}
+                  variant="warning"
+                  className={css.FieldAlert}
+                >
+                  {e}
+                </Alert>
+              ))}
+            </Form.Group>
+
+            <Form.Group className="mb-4" controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={handleChange}
+                name="password"
+              />
+              {fieldErrors.password1?.map((e, index) => (
+                <Alert
+                  key={`${e}-${index}`}
+                  variant="warning"
+                  className={css.FieldAlert}
+                >
+                  {e}
+                </Alert>
+              ))}
+            </Form.Group>
+            {fieldErrors.non_field_errors?.map((e, index) => (
+              <Alert
+                key={`${e}-${index}`}
+                variant="warning"
+                className={css.FieldAlert}
+              >
+                {e}
+              </Alert>
+            ))}
+
+            <Button variant="success" type="submit" className="mt-3">
+              Sign up!
+            </Button>
+          </Form>
+        </Col>
+        <Col className="d-none d-md-flex">
+          <p>Image placeholder</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default SignIn;
