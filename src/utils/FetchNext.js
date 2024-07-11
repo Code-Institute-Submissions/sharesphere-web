9fc379341734998bce20682408f3bea2d5c173e6
Felix Tenggren <felixtenggren1@gmail.com>
2024-07-11 11:28:28 +0000

    Add function to fetch  data for infinite scroll

---
+++ src/utils/FetchNext.js
@@ -0,0 +1,17 @@
+import { axiosInstance } from "../axios/axiosDefaults";
+
+export const FetchNext = async (state, setState) => {
+  try {
+    const { data } = await axiosInstance.get(state.next);
+    setState((prevState) => ({
+      ...prevState,
+      results: [...prevState.results, ...data.results],
+      next: data.next,
+    }));
+    return state;
+  } catch (error) {
+    console.log("Error when fetching more data:", error);
+    return "Something went wrong when loading more data"
+  }
+  
+};
