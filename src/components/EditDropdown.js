73176647fc1c7d49ddd72eae1f8664582cba2c15
Felix Tenggren <felixtenggren1@gmail.com>
2024-07-13 13:33:09 +0000

    Add dropdown component for editing and deleting

---
+++ src/components/EditDropdown.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Dropdown } from "react-bootstrap";
+import css from "../styles/css/EditDropdown.module.css";
+
+// https://github.com/mr-fibonacci/moments/blob/bb6657e265fb18360b841e10d9d633dad06f4e5c/src/components/MoreDropdown.js#L8-L17
+const ThreeDots = React.forwardRef(({ onClick }, ref) => (
+  <i
+    className={`fas fa-ellipsis ${css.ToggleIcon}`}
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  />
+));
+
+export const EditDropdown = () => {
+  return (
+    <Dropdown>
+      <Dropdown.Toggle as={ThreeDots}></Dropdown.Toggle>
+      <Dropdown.Menu className={css.Menu}>
+        <Dropdown.Item aria-label="edit" onClick={() => {}}>
+          Edit
+        </Dropdown.Item>
+        <Dropdown.Item aria-label="delete" onClick={() => {}}>
+          Delete
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
