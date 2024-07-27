634ca0804c0a00863b085cf71220559104245e92
Felix Tenggren <felixtenggren1@gmail.com>
2024-07-27 15:33:09 +0000

    Wrap avatar in div to maintain img aspect ratio

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Image from "react-bootstrap/Image";
+import css from "../styles/css/Avatar.module.css"; // Import the CSS file
 
 const Avatar = ({ src, size, alt }) => {
   return (
-    <Image
-      className="border border-secondary"
-      src={src}
-      height={size}
-      width={size}
-      alt={alt}
-      roundedCircle
-    />
+    <div className={css.AvatarWrapper} style={{ height: size, width: size }}>
+      <Image
+        className={`${css.Avatar} border border-secondary`}
+        src={src}
+        alt={alt}
+        roundedCircle
+      />
+    </div>
   );
 };
 
