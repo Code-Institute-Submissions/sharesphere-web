0488e5d78fc9d88f7dd526e388eb18ed1f205c73
Felix Tenggren <felixtenggren1@gmail.com>
2024-07-29 21:18:55 +0000

    Add comment on limitation of handler usage

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,12 +1,19 @@
-import { rest } from "msw";
+import { http, HttpsResponse } from "msw";
 
 // Taken from:
 // https://github.com/mr-fibonacci/moments/blob/be21d73ba17a071bfb941c9587575c0a9e10615b/src/mocks/handlers.js
-const baseURL = "https://wt-rest-fw-api-3be4d49beb1c.herokuapp.com/";
+const baseURL = "https://sharesphere-8737cda00b1a.herokuapp.com/";
 
 export const handlers = [
-  rest.get(`${baseURL}dj-rest-auth/user/`, () => {
-    return rest.json({
+  /**
+   * This handler is set but not fully utilized in testing yet.
+   * That is because of the how the AuthContext handles the user status
+   * through local storage. So the workaround I've adopted to mock the
+   * logged in status is to set the localStorage loggedInUser object
+   * in the test suite.
+   */
+  http.get(`${baseURL}dj-rest-auth/user/`, () => {
+    return HttpsResponse.json({
       pk: 1,
       username: "adminTony",
       email: "",
@@ -17,7 +24,7 @@ export const handlers = [
         "https://res.cloudinary.com/djmipr2p0/image/upload/v1/media/images/22Q",
     });
   }),
-  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+  http.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
     return res(ctx.status(200));
   }),
 ];
